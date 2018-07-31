ae4f36ecae7b4bed59d6b0e09da1e83f754b996e
jiri.fencl <jiri.fencl@firma.seznam.cz>
2018-08-01 00:18:14 +0200

    faststore start

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
 import React, { Component } from 'react';
 import './App.css';
+import firebase from './config/firebase.js';
 
 class App extends Component {
+  /* ================================================================================== */
+  constructor() {
+    super();
+
+    this.state = {
+      appStatus: 'init',
+      username: '',
+      users: []
+    }
+
+    this._firebaseAuth = firebase.auth().signInAnonymously().catch(function(error) {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.error('signInAnonymously failed', errorCode, errorMessage);
+    });
+
+    this._firebaseDatabase = firebase.database();
+
+    this._firebaseCallback = null;
+  }
+    /* ================================================================================== */
   render() {
     return (
       <div className="App">
